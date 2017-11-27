d4a2f5db570d8f4285422343cfb85702518d187c
Martin <martin.erik.lundgren@gmail.com>
2017-11-27 11:32:08 +0100

    test

---
+++ Solution/Solution/Controllers/AdminController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Solution.Models;
+
+namespace Solution.Controllers
+{
+    //[Authorize]
+    public class AdminController : Controller
+    {
+        private SFI_DBEntities db = new SFI_DBEntities();
+        // GET: Admin
+        public ActionResult Index()
+        {
+            //Test
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Segment segment)
+        {
+            db.Segments.Add(segment);
+            db.SaveChanges();
+            return View();
+        }
+    }
+}
