bbe04be3f09be3e1e81967f9740651cbad831256
Martin <martin.erik.lundgren@gmail.com>
2017-11-28 10:59:23 +0100

    added functionality new segments

--- Solution/Solution/Controllers/AdminController.cs
+++ Solution/Solution/Controllers/AdminController.cs
@@ -21,12 +21,14 @@ namespace Solution.Controllers
 
         public ActionResult newSegment()
         {
-            //TODO: add functionality
+            //TODO: Maybe use a View model? But this works 
             var segments = (from s in db.Segments
                             orderby s.Name
                             select s).ToList();
+            ViewBag.segments = segments;
             return View();
         }
+
         [HttpPost]
         public ActionResult newSegment(Segment segment)
         {
@@ -36,7 +38,7 @@ namespace Solution.Controllers
                 {
                     db.Segments.Add(segment);
                     db.SaveChanges();
-                    return RedirectToAction("Index", "Admin");
+                    return RedirectToAction("newSegment", "Admin");
                 }
                 //TODO: Add exception, maybe custom error page? 
                 catch (Exception)
