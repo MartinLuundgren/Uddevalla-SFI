84162f26975f6ac2770cedbc2f98cbc4e1b0dcea
Martin <Martin.erik.lundgren@gmail.com>
2017-12-13 14:15:22 +0100

    test

--- Solution/Solution/Controllers/HomeController.cs
+++ Solution/Solution/Controllers/HomeController.cs
@@ -50,8 +50,6 @@ namespace Solution.Controllers
                 var getAssignments = (from a in db.Assignments
                                     where a.Categories_ID == id
                                     select a).ToList();
-                if (getAssignments.Count > 0)
-                {
                     foreach (var item in getAssignments)
                     {
                         if (item.Answer_One != null)
@@ -91,7 +89,6 @@ namespace Solution.Controllers
                         }
                     }
                     return View(getAssignments);
-                }
             }
             catch
             {
